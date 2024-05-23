54f79d5a5a16fbb11c11c5738865c5f7893651f2
João Afonso Katombela <ja3328173@gmail.com>
2024-05-23 13:29:37 +0100

    first commit

---
+++ src/constants/images.ts
@@ -0,0 +1,10 @@
+const images = {
+  logo_f: require("../assets/images/logo-f.png"),
+  logo: require("../assets/images/digikalaLogo.png"),
+  avt: require("../assets/images/avatar.jpg"),
+  logKey: require("../assets/images/Reset password-pana.svg"),
+  dashboard: require("../assets/images/Revenue-cuate.svg"),
+  notFound: require("../assets/images/Oops 404 Error with a broken robot-cuate.svg"),
+};
+
+export default images;
