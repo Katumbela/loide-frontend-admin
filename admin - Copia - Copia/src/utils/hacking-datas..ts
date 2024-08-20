a66777e904bfc361583b17749ec1c1c7aac7ea9f
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-20 18:55:40 +0100

    feat: admin - Copia - Copia/src/utils/hacking-datas..ts. Criação de novo arquivo

---
+++ admin - Copia - Copia/src/utils/hacking-datas..ts
@@ -0,0 +1,38 @@
+
+// src/types.ts
+
+export interface IHacking {
+    nome: string;
+    desafiosResolvidos: number;
+    pontuacao: number;
+}
+
+
+
+export const datasRaning: IHacking[] = [
+    { nome: 'Alice example name', desafiosResolvidos: 15, pontuacao: 350 },
+    { nome: 'Bob example name', desafiosResolvidos: 12, pontuacao: 300 },
+    { nome: 'Carol example name', desafiosResolvidos: 10, pontuacao: 280 },
+    { nome: 'David example name', desafiosResolvidos: 18, pontuacao: 380 },
+    { nome: 'Eve example name', desafiosResolvidos: 20, pontuacao: 400 },
+    { nome: 'Alice example name', desafiosResolvidos: 15, pontuacao: 350 },
+    { nome: 'Bob example name', desafiosResolvidos: 12, pontuacao: 300 },
+    { nome: 'Carol example name', desafiosResolvidos: 10, pontuacao: 280 },
+    { nome: 'David example name', desafiosResolvidos: 18, pontuacao: 380 },
+    { nome: 'Eve example name', desafiosResolvidos: 20, pontuacao: 400 },
+    { nome: 'Alice example name', desafiosResolvidos: 15, pontuacao: 350 },
+    { nome: 'Bob example name', desafiosResolvidos: 12, pontuacao: 300 },
+    { nome: 'Carol example name', desafiosResolvidos: 10, pontuacao: 280 },
+    { nome: 'David example name', desafiosResolvidos: 18, pontuacao: 380 },
+    { nome: 'Eve example name', desafiosResolvidos: 20, pontuacao: 400 },
+    { nome: 'Alice example name', desafiosResolvidos: 15, pontuacao: 350 },
+    { nome: 'Bob example name', desafiosResolvidos: 12, pontuacao: 300 },
+    { nome: 'Carol example name', desafiosResolvidos: 10, pontuacao: 280 },
+    { nome: 'David example name', desafiosResolvidos: 18, pontuacao: 380 },
+    { nome: 'Eve example name', desafiosResolvidos: 20, pontuacao: 400 },
+    { nome: 'Alice example name', desafiosResolvidos: 15, pontuacao: 350 },
+    { nome: 'Bob example name', desafiosResolvidos: 12, pontuacao: 300 },
+    { nome: 'Carol example name', desafiosResolvidos: 10, pontuacao: 280 },
+    { nome: 'David example name', desafiosResolvidos: 18, pontuacao: 380 },
+    { nome: 'Eve example name', desafiosResolvidos: 20, pontuacao: 400 },
+];
