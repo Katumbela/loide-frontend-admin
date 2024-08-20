33079ce686ece0fa31d55d60b68abd840f49b7bd
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-20 18:55:25 +0100

    feat: admin - Copia - Copia/src/presentation/pages/sidebar-pages/news-amin-page.tsx. Criação de novo arquivo

---
+++ admin - Copia - Copia/src/presentation/pages/sidebar-pages/news-amin-page.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { useAuth } from '@/context/auth-context';
+import { Navbar } from '@/presentation/components/dashboard-components/navbar/navbar';
+import { Sidebar } from '@/presentation/components/dashboard-components/sidebar/sidebar';
+import AddNewsPage from '../admin-pages/add-news-page';
+
+
+export const NewsAdminPage: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  const { userSettings } = useAuth();
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`${userSettings.darkMode ? 'dark' : ''}`}>
+      <Navbar />
+      <Sidebar toggleSidebar={toggleSidebar} isOpen={isOpen} />
+      <div className={`min-h-screen p-4 sm:pt-20 ${isOpen ? 'sm:ml-[10rem]' : 'sm:ml-[4rem]'} transition-all duration-300 bg-gray-100 dark:bg-gray-900`}>
+        <AddNewsPage />
+      </div>
+    </div>
+  );
+};
