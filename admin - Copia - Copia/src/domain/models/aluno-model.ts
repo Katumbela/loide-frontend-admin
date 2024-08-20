6ccca0cf33f52c9de6f53ca8bf1342f1428aef15
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-20 18:54:36 +0100

    feat: admin - Copia - Copia/src/domain/models/aluno-model.ts. Criação de novo arquivo

---
+++ admin - Copia - Copia/src/domain/models/aluno-model.ts
@@ -0,0 +1,16 @@
+export interface MAluno {
+    displayName: string;
+    email: string;
+    phoneNumber: string;
+    cod_aluno?: number;
+    photoURL: string;
+    country?: string
+    company?: string
+    role?: string
+    address?: string
+    fb?: string
+    tw?: string
+    in?: string
+    insta?: string
+  }
+  
