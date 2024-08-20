bb2a0ad1f3d49fe080b2edf0259dc4b71215edeb
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-20 18:41:48 +0100

    feat: admin/src/domain/config/testimonials-config.ts. Criação de novo arquivo

---
+++ admin/src/domain/config/testimonials-config.ts
@@ -0,0 +1,54 @@
+import { ITestimonials } from "../../interfaces/testimonials/testimonials";
+import { users } from "../../utils/image-exporter";
+
+
+
+export const TestimonialsData: ITestimonials[] = [
+    {
+      id: 1,
+      img: users.user_9,
+      name: "Nadiusk Santana",
+      content: "Espero que haja uma combinação de conteúdos abrangentes, atualizados e acessíveis para todos os níveis de conhecimento.",
+      role: "CyberTM | PORTUGAL",
+    },
+    {
+      id: 2,
+      img: users.user_10,
+      name: "João Barroso",
+      content:
+        "A Hakyoff deve manter contato próximo com seus alunos, incentivando-os a adquirir os conhecimentos oferecidos.",
+      role: "Pentester | BRASIL",
+    },
+    {
+      id: 3,
+      img: users.user_11,
+      name: "Idalécio Silva",
+      content:
+        "Espero que seja uma plataforma revolucionária para o treinamento de profissionais e entusiastas em segurança cibernética.",
+      role: "Project Manager | ANGOLA",
+    },
+    {
+      id: 4,
+      img: users.user_12,
+      name: "Jagat Singh",
+      content:
+        "Espero que a Hakyoff ofereça treinamentos inovadores e práticos, capacitando profissionais em cibersegurança.",
+      role: "Security Researcher | ÍNDIA",
+    },
+    {
+      id: 5,
+      img: users.user_13,
+      name: "Joaquim Timóteo",
+      content:
+        "Espero que a Hakyoff ofereça um ambiente prático e dinâmico para aprimorar habilidades em cibersegurança.",
+      role: "Cybersecurity Analyst | RÚSSIA",
+    },
+    {
+      id: 6,
+      img: users.user_14,
+      name: "Charlene Medida",
+      content:
+        "Espero que a Hakyoff simule cenários reais e imersivos , promovendo aprendizado prático e eficaz.",
+      role: "Machine Learn Engineer | CAMEROON",
+    },
+  ];
