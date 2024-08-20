36aa3868910ff6395b4bf3328919722c47117930
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-20 18:55:28 +0100

    feat: admin - Copia - Copia/src/presentation/pages/soon/soon.tsx. Criação de novo arquivo

---
+++ admin - Copia - Copia/src/presentation/pages/soon/soon.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export function SoonPage() {
+    document.title = 'Login to the Academy';
+
+
+    return (
+        <div className="relative flex items-center justify-center h-screen login-bg">
+
+            <center>
+                <h2 className="text-white text-3xl">Brevemente disponível !</h2>
+            </center>
+
+        </div>
+    );
+}
