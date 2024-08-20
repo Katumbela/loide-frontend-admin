672bcd2a67c07591d18e869b6aad7b2aed58ae40
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-20 18:55:45 +0100

    feat: admin - Copia - Copia/src/utils/tabs-utils.tsx. Criação de novo arquivo

---
+++ admin - Copia - Copia/src/utils/tabs-utils.tsx
@@ -0,0 +1,9 @@
+
+import { ITraining } from "@/interfaces/training/training";
+import { IBenefits } from "@/interfaces/benefits/benefits";
+
+export interface TU {
+  course: ITraining,
+  aulas: IBenefits
+}
+
