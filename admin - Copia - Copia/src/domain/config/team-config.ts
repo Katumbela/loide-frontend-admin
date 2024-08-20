c9834df8ff4c12f535bc509c00f68a27087c6a04
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-20 18:54:33 +0100

    feat: admin - Copia - Copia/src/domain/config/team-config.ts. Criação de novo arquivo

---
+++ admin - Copia - Copia/src/domain/config/team-config.ts
@@ -0,0 +1,84 @@
+import { ITeam } from "../../interfaces/team/team";
+import { users } from "../../utils/image-exporter";
+
+export const TeamDatas: ITeam[] = [
+  {
+    id: 1,
+    name: "Valdemar Oliveira",
+    role: "CEO | Founder",
+    description: "Lorem Ipsum is simply dummy text  .Lorem Ipsum is simply dummy text  .Lorem Ipsum is simply dummy text  .",
+    picture: users.user_7,
+    midias: {
+      facebook: "https://facebook.com/nome",
+      linkedin: "https://linkedin.com/in/nome",
+      twitter: "https://twitter.com/nome",
+      instagram: "https://instagram.com/nome",
+    },
+  },
+  {
+    id: 2,
+    name: "João Katombela",
+    role: "Fullstack Developer",
+    description: "Lorem Ipsum is simply dummy text  .Lorem Ipsum is simply dummy text  .Lorem Ipsum is simply dummy text  .",
+
+    picture: users.user_3,
+    midias: {
+      facebook: "https://facebook.com/j.a.katombela",
+      linkedin: "https://linkedin.com/in/joao-afonso-katumbela",
+      twitter: "https://twitter.com/joao_katumbela",
+      instagram: "https://instagram.com/afonso.katumbela",
+    },
+  },
+  {
+    id: 3,
+    name: "Walt de Jesus",
+    role: "UI/UX Desiger",
+    description: "Lorem Ipsum is simply dummy text  .Lorem Ipsum is simply dummy text  .Lorem Ipsum is simply dummy text  .",
+    picture: users.user_1,
+    midias: {
+      facebook: "https://facebook.com/nome",
+      linkedin: "https://linkedin.com/in/nome",
+      twitter: "https://twitter.com/nome",
+      instagram: "https://instagram.com/nome",
+    },
+  },
+  {
+    id: 4,
+    name: "João Estulene",
+    role: "Penetration Tester",
+    description: "Lorem Ipsum is simply dummy text  .Lorem Ipsum is simply dummy text  .Lorem Ipsum is simply dummy text  .",
+    picture: users.user_4,
+    midias: {
+      facebook: "https://facebook.com/nome",
+      linkedin: "https://linkedin.com/in/nome",
+      twitter: "https://twitter.com/nome",
+      instagram: "https://instagram.com/nome",
+    },
+  },
+  {
+    id: 6,
+    name: "Jacila da Silveiro",
+    role: "Dpt Marketing",
+    description: "Lorem Ipsum is simply dummy text  .Lorem Ipsum is simply dummy text  .Lorem Ipsum is simply dummy text  .",
+    picture: users.user_6,
+    midias: {
+      facebook: "https://facebook.com/nome",
+      linkedin: "https://linkedin.com/in/nome",
+      twitter: "https://twitter.com/nome",
+      instagram: "https://instagram.com/nome",
+    },
+  },
+  {
+    id: 5,
+    name: "Mauro Luanda",
+    role: "Dpt Marketing",
+    description: "Lorem Ipsum is simply dummy text  .Lorem Ipsum is simply dummy text  .Lorem Ipsum is simply dummy text  .",
+    picture: users.user_5,
+    midias: {
+      facebook: "https://facebook.com/nome",
+      linkedin: "https://linkedin.com/in/nome",
+      twitter: "https://twitter.com/nome",
+      instagram: "https://instagram.com/nome",
+    },
+  },
+];
