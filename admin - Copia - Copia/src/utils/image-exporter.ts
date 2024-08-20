b0a34f13a304d41eebd5155e0ea8a01c19e62424
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-20 18:55:40 +0100

    feat: admin - Copia - Copia/src/utils/image-exporter.ts. Criação de novo arquivo

---
+++ admin - Copia - Copia/src/utils/image-exporter.ts
@@ -0,0 +1,183 @@
+// exportar os icons
+
+import logo from "../assets/logos/logo.png";
+import logo_2 from "../assets/logos/logo-2.png";
+import taca from "../assets/icons/cup.png";
+import check_list from "../assets/icons/check-list.png";
+import pin_btn from "../assets/icons/pin-btn.png";
+import notes from "../assets/icons/note-2.png";
+import pcb from "../assets/icons/pcb.png";
+import user_tag from "../assets/icons/user-tag.png";
+import fb from "../assets/icons/facebook.png";
+import insta from "../assets/icons/insta.png";
+import yt from "../assets/icons/youtub.png";
+import secur from "../assets/icons/secur.png";
+import people from "../assets/icons/people.png";
+import apos from "../assets/icons/apos.png";
+import burger from "../assets/icons/burger-menu-5.svg";
+import energy from "../assets/icons/energy.png";
+import time from "../assets/icons/time.png";
+import lab from "../assets/icons/laboratory-scientist-svgrepo-com.png";
+import exclamation from "../assets/icons/exclamation.png";
+import linkedin from "../assets/icons/linkedin.png";
+
+//exportar os bgs
+
+import bg_card from "../assets/bgs/bg-card.png";
+import bg_certificate_filled from "../assets/bgs/Certficado.png";
+import bg_certificate from "../assets/bgs/Certficado_template.svg";
+import bg_lab_chall from "../assets/bgs/bg-lab-challeng.png";
+import bg_hero from "../assets/bgs/hero.png";
+import bg_student from "../assets/bgs/bg-student.jpg";
+import bg_2 from "../assets/bgs/fundo 2.png";
+import bg_hero_fundo from "../assets/bgs/bg_hero_fundo.png";
+import bg_newsletter from "../assets/bgs/newsletter.png";
+import bg_video from "../assets/bgs/video.png";
+import bg_lab from "../assets/bgs/bg-lab.jpg";
+import diploma from "../assets/icons/diploma-svgrepo-com.png";
+
+// importar os gifs
+
+import scroll_bottom from "../assets/gifs/anim_scroll_bottom.gif";
+
+// importar os svgs
+
+import play_svg from "../assets/svgs/play-circle-svgrepo-com.svg";
+import tocha_svg from "../assets/svgs/torch-svgrepo-com.svg";
+import unauthorized_svg from "../assets/svgs/unauthorized-error.svg";
+import not_found_svg from "../assets/svgs/not-found-error.svg";
+import bg_placeholder_svg from "../assets/svgs/Placeholder-_-Glossary.svg";
+import ouro_svg from "../assets/svgs/ouro-trophy-svgrepo-com.svg";
+import award_trophy_svg from "../assets/svgs/award-trophy-svgrepo-com.svg";
+import silver_trophy_svg from "../assets/svgs/silver-ribbon-svgrepo-com.svg";
+import bronze_trophy_svg from "../assets/svgs/bronze-ribbon-label-svgrepo-com.svg";
+import gold_trophy_svg from "../assets/svgs/gold-trophy-material-4-svgrepo-com.svg";
+import box_svg from "../assets/svgs/diagram-module-library-svgrepo-com.svg";
+import email_confirm_svg from "../assets/svgs/email-inbox-letter-svgrepo-com.svg";
+import square_svg from "../assets/svgs/module-svgrepo-com.svg";
+import module_svg from "../assets/svgs/diagram-module-library-svgrepo-com.svg";
+
+// exportar os users
+
+import user_default from "../assets/users/default.jpg";
+import user_1 from "../assets/users/user 1.png";
+import user_2 from "../assets/users/user 2.png";
+import user_3 from "../assets/users/user 3.png";
+import user_4 from "../assets/users/user 4.png";
+import user_5 from "../assets/users/user 5.png";
+import user_6 from "../assets/users/user 6.png";
+import user_7 from "../assets/users/user 7.png";
+import user_8 from "../assets/users/user 8.png";
+import user_9 from "../assets/users/user 9.png";
+import user_10 from "../assets/users/user 10.png";
+import user_11 from "../assets/users/user 11.png";
+import user_12 from "../assets/users/user 12.png";
+import user_13 from "../assets/users/user 13.png";
+import user_14 from "../assets/users/user 14.png";
+
+//importar os videos and extras 
+
+import video1 from "../assets/video/video-test.mp4";
+import bg_music from "../assets/extras/bg-music.jpg";
+import bg_cert from "../assets/extras/hacker_cert.png";
+import bg_card_ctfs from "../assets/extras/bg-card-ctfs.jpg";
+import hack_face from "../assets/extras/hack-face.png";
+import hack_face_white from "../assets/extras/hack-face-white.png";
+import loader_circle from "../assets/extras/progress.gif";
+import secure_pay from "../assets/extras/353-3532675_stripe-secure-payment-logo-hd-png-download.png";
+
+
+
+export const bg = {
+  bg_card,
+  bg_hero,
+  bg_certificate_filled,
+  bg_newsletter,
+  bg_certificate,
+  bg_lab_chall,
+  bg_video,
+  bg_student,
+  bg_hero_fundo,
+  bg_lab,
+  bg_2
+};
+
+export const extra = {
+  bg_music,
+  bg_card_ctfs,
+  hack_face,
+  hack_face_white,
+  secure_pay,
+  loader_circle,
+  bg_cert
+};
+
+export const logos = {
+  logo,
+  logo_2,
+};
+
+export const svgs = {
+  play_svg,
+  module_svg,
+  gold_trophy_svg,
+  bronze_trophy_svg,
+  box_svg,
+  award_trophy_svg,
+  silver_trophy_svg,
+  bg_placeholder_svg,
+  unauthorized_svg,
+  not_found_svg,
+  tocha_svg,
+  email_confirm_svg,
+  square_svg, ouro_svg
+
+};
+
+export const videos = {
+  video1,
+};
+
+export const icons = {
+  taca,
+  pin_btn,
+  pcb,
+  check_list,
+  energy,
+  lab,
+  diploma,
+  burger,
+  time,
+  people,
+  secur,
+  fb,
+  apos,
+  linkedin,
+  exclamation,
+  yt,
+  insta,
+  notes,
+  user_tag,
+};
+
+export const users = {
+  user_1,
+  user_2,
+  user_default,
+  user_3,
+  user_4,
+  user_5,
+  user_6,
+  user_7,
+  user_8,
+  user_9,
+  user_10,
+  user_11,
+  user_12,
+  user_13,
+  user_14,
+};
+
+export const gifs = {
+  scroll_bottom,
+};
