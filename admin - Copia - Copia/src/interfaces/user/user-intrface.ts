e2ef1f0573a13085916ab1fed58e318d02301857
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-20 18:54:47 +0100

    feat: admin - Copia - Copia/src/interfaces/user/user-intrface.ts. Criação de novo arquivo

---
+++ admin - Copia - Copia/src/interfaces/user/user-intrface.ts
@@ -0,0 +1,12 @@
+export interface IUser {
+  id: number;
+  name: string;
+  email: string;
+  device: string;
+  phone?: number;
+  address?: string;
+  role: string;
+  created_at: string;
+  updated_at: string;
+}
+ 
